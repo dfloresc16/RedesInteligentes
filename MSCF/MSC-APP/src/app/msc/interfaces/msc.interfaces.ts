cf82cdaf99464e5d0731fcb563f6116cded7fa81
Diego Flores <dfloresc1602@alumno.ipn.mx>
2023-06-08 13:17:46 -0600

    Subiendo tailwind.config.js

---
+++ MSCF/MSC-APP/src/app/msc/interfaces/msc.interfaces.ts
@@ -0,0 +1,10 @@
+export interface UsuarioHlrDTO{
+  idUsuario:String,
+  numAbonado:String,
+  mnc:Number,
+  tipoPlan:Number,
+  idEquipo:Number,
+  imei:String
+}
+
+
---
+++ MSCF/MSC-APP/src/app/msc/pages/login/login-page.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-login',
+  templateUrl: './login-page.component.html'
+})
+export class LoginComponent {
+
+}
