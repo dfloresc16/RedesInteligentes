1255f2523adda0f0c5cf9a561afb482a0532cff6
Diego Flores <dfloresc1602@alumno.ipn.mx>
2023-06-08 12:51:00 -0600

    Estilos Tailwind agregados

--- MSC/src/main/java/com/redesint/DTO/UsuarioHrlDTO.java
+++ MSC/src/main/java/com/redesint/DTO/UsuarioHrlDTO.java
@@ -21,8 +21,6 @@ public class UsuarioHrlDTO {
 	
 	private String imei;
 	
-	
-	
 	/**
 	 * 
 	 */
--- MSCF/MSC-APP/src/app/app.module.ts
+++ MSCF/MSC-APP/src/app/app.module.ts
@@ -1,14 +1,19 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
+
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './msc/pages/login/login-page.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    CommonModule
   ],
   providers: [],
   bootstrap: [AppComponent]
